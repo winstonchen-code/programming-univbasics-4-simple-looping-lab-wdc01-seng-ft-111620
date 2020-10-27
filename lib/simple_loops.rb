d877c622f2cfbc2dd0f37c5a22ac71624856962b
Winston Chen <winstonchen.wm@gmail.com>
2020-10-27 18:48:53 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,13 +24,11 @@ end
 
 def return_string_array(array)
   count = 0 
-  new_array = []
   while count < array.length do
-    new_array.push(array[count].to_s)
-new_array << array[count].to_s
+    array[count] = array[count].to_s
     count += 1 
   end
-  new_array
+  array
 end
   
   
